9cdaac03df74ffac68965e8c358ba564607a0bb8
João Rafael <jrfifipi@gmail.com>
2024-11-17 14:47:32 -0300

    Ajustes necessários na página de dentistas

--- server.js
+++ server.js
@@ -12,9 +12,11 @@ app.set('view engine' , 'ejs')
 PORT = 3000
 
 // basicamente, toda vez que usar localhost:3000/, vai retornar o index.ejs, que é só o cabeçalho, padrão
-app.get("/" ,(req , res) => {
-    res.render("index")    
-});
+
+app.get('/' , (req , res) =>{
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+}) 
+
 app.get("/cadastro" ,(req , res) => {
     res.render("cadastro")    
 });
